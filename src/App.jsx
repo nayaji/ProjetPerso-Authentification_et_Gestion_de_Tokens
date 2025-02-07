65e1b2576320163cda8e5e5896fa258d0fa9fab3
nayaji <najtt89@gmail.com>
2025-02-07 23:57:36 +0100

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
